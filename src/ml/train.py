50c35c00cfbdbc844c592158ddba5c518c959896
LowIQCoder <marselberheev@mail.ru>
2025-10-10 12:15:24 +0300

    Small fixes

--- src/ml/train.py
+++ src/ml/train.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from torch import optim
 from sklearn.metrics import f1_score
+import os
 
 from tqdm import tqdm
 
@@ -19,9 +20,11 @@ def train(
     epochs,
     device,
     checkpoint
-) -> None:
-    best_acc = 0
+) -> None:    
+    if not os.path.exists("./data/models/"):
+        os.makedirs("./data/models/")
 
+    best_acc = 0
     if checkpoint:
         try:
             model.load_state_dict(torch.load(checkpoint))
@@ -155,7 +158,7 @@ if __name__ == "__main__":
         loss_fn=loss,
         train_loader=train_loader,
         val_loader=val_loader,
-        epochs=2,
+        epochs=10,
         device=device,
         checkpoint="./data/models/best_model.pth"
     )
